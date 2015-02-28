2b247262b13b5ca3414ec495f41562a0a7439c4c
Anton Zhirkov <an.zhirkov@gmail.com>
2015-02-28 16:13:08 +0300

    Added CRUD and validations for Card

--- app/models/card.rb
+++ app/models/card.rb
@@ -1,9 +1,12 @@
 class Card < ActiveRecord::Base
-  before_create :record_review_date
+  validates :original_text, :translated_text, :review_date, presence: true
+  validate :original_text_cannot_be_equal_translated_text
 
   private
 
-  def record_review_date
-    self.review_date = Time.now
+  def original_text_cannot_be_equal_translated_text
+    if self.original_text.mb_chars.downcase.to_s == self.translated_text.mb_chars.downcase.to_s
+      errors.add(:translated_text, "Не может быть такой же как оригинальный текст")
+    end
   end
 end
