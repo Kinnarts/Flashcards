28ba27214fdcac6ef1fb8f58127912dc51d26c56
Anton Zhirkov <an.zhirkov@gmail.com>
2015-02-28 20:09:00 +0300

    Delete review_date from _form.html.erb and set @card as local variable

--- app/models/card.rb
+++ app/models/card.rb
@@ -1,8 +1,13 @@
 class Card < ActiveRecord::Base
-  validates :original_text, :translated_text, :review_date, presence: true
+  before_create  :record_review_date
+  validates :original_text, :translated_text, presence: true
   validate :original_text_cannot_be_equal_translated_text
 
   private
+
+  def record_review_date
+    self.review_date = Time.now + (60 * 60 * 72)
+  end
 
   def original_text_cannot_be_equal_translated_text
     if self.original_text.mb_chars.downcase.to_s == self.translated_text.mb_chars.downcase.to_s
