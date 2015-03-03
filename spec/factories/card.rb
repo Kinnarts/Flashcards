00cc34f08f1cc73d9d0a0eef440578f1c9802fac
Anton Zhirkov <an.zhirkov@gmail.com>
2015-03-03 14:06:39 +0300

    Divided feature spec and used let! in review_srec.rb

--- spec/factories/card.rb
+++ spec/factories/card.rb
@@ -3,6 +3,6 @@ FactoryGirl.define do
   factory :card do
     original_text Faker::Lorem.word
     translated_text Faker::Lorem.word
-    review_date Time.now - 3.days
+    review_date Time.now + 3.days
   end
 end
--- spec/features/card_management_spec.rb
+++ spec/features/card_management_spec.rb
@@ -1,5 +1,5 @@
 require "rails_helper"
-describe "Card" do
+describe "Card management" do
   before(:all) do
     @card = create(:card)
   end
@@ -27,24 +27,5 @@ describe "Card" do
     visit cards_path
     click_link "Редактировать"
     expect(page).to have_content "Введите новые данные"
-  end
-  
-  context "review card" do
-    before(:each) do
-      Card.first.update_attributes(review_date: Time.now - 2.days)
-      click_link "Флешкарточкер"
-    end
-
-    it "wrong" do
-      fill_in "Введите перевод", with: "12345"
-      click_button "Проверить"
-      expect(page).to have_content "Ответ неверный, может повезет в следующий раз..."
-    end
-
-    it "succes" do
-      fill_in "Введите перевод", with: @card.translated_text
-      click_button "Проверить"
-      expect(page).to have_content "Поздравляем, все верно!"
-    end
   end
 end
