ce0593dd16d008408dbaf7cb0822bbad85b07264
Anton Zhirkov <an.zhirkov@gmail.com>
2015-03-03 18:14:28 +0300

    Change card factory for repair tests

--- spec/factories/card.rb
+++ spec/factories/card.rb
@@ -2,7 +2,7 @@ require "faker"
 FactoryGirl.define do
   factory :card do
     original_text Faker::Lorem.word
-    translated_text Faker::Lorem.word
+    translated_text Faker::Lorem.sentence(word_count = 1)
     review_date Time.now + 3.days
     association :user
   end
